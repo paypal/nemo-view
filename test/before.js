266605caee802cb985278a6229c813af1e94d570
Matt Edelman <medelman@paypal.com>
2014-05-28 08:54:32 -0700

    clean up nemoData

--- test/before.js
+++ test/before.js
@@ -2,12 +2,7 @@ before(function(done) {
 	process.env.nemoData = JSON.stringify({
 		"targetBaseUrl": "http://warm-river-3624.herokuapp.com/",
 		"targetBrowser": "phantomjs",
-		"autoBaseDir": process.cwd() + "/test",
-		// "serverProps": {
-		// 	"port": 4444
-		// },
-		// "targetServer": "localhost",
-		// "seleniumJar": "/usr/local/bin/selenium-server-standalone.jar"
+		"autoBaseDir": process.cwd() + "/test"
 	});
 	done();
 });
