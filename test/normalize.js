344d93dc72443937d86b26ee8755a20c7e5e6e37
Nilesh Kulkarni <nikulkarni@paypal.com>
2015-12-13 23:57:28 -0500

    tests for invalid locators

--- test/normalize.js
+++ test/normalize.js
@@ -45,4 +45,91 @@ describe('nemo-view @normalize@ module', function () {
     });
     done();
   });
+  it('should correctly throw error @notype@', function (done) {
+    var noType = {
+      "noType": {
+        "locator": "foo"
+      }
+    };
+    try {
+      normalize(nemo, noType);
+      done(new Error('Expected error for locator with no type'));
+    } catch (e) {
+      console.log(e);
+      done();
+    }
+  });
+  it('should correctly throw error @emptyType@', function (done) {
+    var emptyType = {
+      "noType": {
+        "locator": "foo",
+        "type": ""
+      }
+    };
+    try {
+      normalize(nemo, emptyType);
+      done(new Error('Expected error for locator with empty type'));
+    } catch (e) {
+      console.log(e);
+      done();
+    }
+  });
+  it('should correctly throw error @blankType@', function (done) {
+    var blankType = {
+      "noType": {
+        "locator": "foo",
+        "type": "  "
+      }
+    };
+    try {
+      normalize(nemo, blankType);
+      done(new Error('Expected error for locator with blank type'));
+    } catch (e) {
+      console.log(e);
+      done();
+    }
+  });
+  it('should correctly throw error @invalidType@', function (done) {
+    var invalidType = {
+      "noType": {
+        "locator": "foo",
+        "type": "bar"
+      }
+    };
+    try {
+      normalize(nemo, invalidType);
+      done(new Error('Expected error for locator with invalid type'));
+    } catch (e) {
+      console.log(e);
+      done();
+    }
+  });
+  it('should correctly throw error @noLocatorValidType@', function (done) {
+    var noLocatorValidType = {
+      "noType": {
+        "type": "css"
+      }
+    };
+    try {
+      normalize(nemo, noLocatorValidType);
+      done(new Error('Expected error for no locator with valid type'));
+    } catch (e) {
+      console.log(e);
+      done();
+    }
+  });
+  it('should correctly throw error @noLocatorInvalidType@', function (done) {
+    var noLocatorInvalidType = {
+      "noType": {
+        "type": "bar"
+      }
+    };
+    try {
+      normalize(nemo, noLocatorInvalidType);
+      done(new Error('Expected error for locator with invalid type'));
+    } catch (e) {
+      console.log(e);
+      done();
+    }
+  });
 });
