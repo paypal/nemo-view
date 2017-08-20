e6e2e05ab992588959bd920432b49c6a8a0ba2f0
Kulkarni, Nilesh(nikulkarni) <nikulkarni@paypal.com>
2017-08-19 18:08:31 -0700

    es6 changes
    more es6 changes
    
    more es6 changes
    
    removed console log

--- test/plugin/shared-fn-plugin.js
+++ test/plugin/shared-fn-plugin.js
@@ -5,32 +5,32 @@ module.exports = {
 			'view': {},
 			'locator': {}
 		};
-		var loginLocator = {
-			"email": {
-				"locator": "email",
-				"type": "id"
-			},
-			"password": {
-				"locator": "password",
-				"type": "id"
-			},
-			"loginButton": {
-				"locator": "input[type='button'][class='login']",
-				"type": "css"
-			},
-      "logoutButton": {
-        "locator": "input[type='button'][class='logout']",
-        "type": "css"
-      }
+        var loginLocator = {
+            "email": {
+                "locator": "email",
+                "type": "id"
+            },
+            "password": {
+                "locator": "password",
+                "type": "id"
+            },
+            "loginButton": {
+                "locator": "input[type='button'][class='login']",
+                "type": "css"
+            },
+            "logoutButton": {
+                "locator": "input[type='button'][class='logout']",
+                "type": "css"
+            }
 
-		};
+        };
 
 
     //setting the second param to "false" means this view won't impose on whatever is already attached to nemo.view
-		login.view.login = nemo.view.addView(loginLocator, ['doesntmatter'], false);
-    login.getPage = function () {
-      return nemo.driver.get(nemo.data.baseUrl + '/login');
-    };
+        login.view.login = nemo.view.addView(loginLocator, ['doesntmatter'], false);
+        login.getPage = function () {
+            return nemo.driver.get(nemo.data.baseUrl + '/login');
+        };
 		login.login = function(email, password) {
 			var me = login.view.login;
 
