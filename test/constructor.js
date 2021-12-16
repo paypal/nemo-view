f3f5ea11e5944b46a54b94f28096de73a3770354
Alex <jianzheng@paypal.com>
2021-12-16 14:51:51 +0800

    change test to use async await

--- test/constructor.js
+++ test/constructor.js
@@ -6,77 +6,58 @@ var Nemo = require('nemo-core'),
   nemo = {};
 
 describe('nemo-view @constructor@', function () {
-  it('should do ? with malformed JSON file(s)', function (done) {
-    nemo = Nemo({
+  it('should do ? with malformed JSON file(s)', async function () {
+    nemo = await Nemo({
       'plugins': {
         "view": {
           "module": "path:../",
           "arguments": ["path:mocks/badjson"]
         }
       }
-    }, function (err) {
-      if (err) {
-        done();
-      } else {
-        done(new Error('should have got an error in nemo callback'));
-      }
+    }).then(() => {
+      throw new TypeError('should have got an error in nemo callback');
+    }).catch(err => {
+      if(err instanceof TypeError)
+        throw new Error('should have got an error in nemo callback');
     });
   });
-  it('should give back _ methods with empty locator directory', function (done) {
-    nemo = Nemo({
+  it('should give back _ methods with empty locator directory', async function () {
+    nemo = await Nemo({
       'plugins': {
         "view": {
           "module": "path:../",
           "arguments": ["path:mocks/empty"]
         }
       }
-    }, function (err) {
-      if (err) {
-        done(new Error('shouldnt have got an error in nemo callback'));
-      } else {
-        assert(nemo.view);
-        assert(nemo.view._find);
-        done();
-      }
-      nemo.driver.quit();
     });
+    assert(nemo.view);
+    assert(nemo.view._find);
+    await nemo.driver.quit()
   });
-  it('should give back _ methods with empty locator directory', function (done) {
-    nemo = Nemo({
+  it('should give back _ methods with empty locator directory', async function () {
+    nemo = await Nemo({
       'plugins': {
         "view": {
           "module": "path:../",
           "arguments": ["path:mocks/idontexist"]
         }
       }
-    }, function (err) {
-      if (err) {
-        done(new Error('shouldnt have got an error in nemo callback'));
-      } else {
-        assert(nemo.view);
-        assert(nemo.view._find);
-        done();
-      }
-      nemo.driver.quit();
     });
+    assert(nemo.view);
+    assert(nemo.view._find);
+    await nemo.driver.quit()
   });
-  it('should give back _ methods with no locatorDirectory argument', function (done) {
-    nemo = Nemo({
+  it('should give back _ methods with no locatorDirectory argument', async function () {
+    nemo = await Nemo({
       'plugins': {
         "view": {
           "module": "path:../",
           "arguments": null
         }
       }
-    }, function (err) {
-      if (err) {
-        done(new Error('shouldnt have got an error in nemo callback'));
-      } else {
-        assert(nemo.view);
-        assert(nemo.view._find);
-        done();
-      }
-      nemo.driver.quit();
     });
+    assert(nemo.view);
+    assert(nemo.view._find);
+    await nemo.driver.quit()
   });
 });
