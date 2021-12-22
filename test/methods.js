592aff9d74ace12915ad7e19f9b70436ecfddf90
Alex <jianzheng@paypal.com>
2021-12-22 16:39:13 +0800

    refine to use async/await

--- test/methods.js
+++ test/methods.js
@@ -8,77 +8,99 @@ var assert = require('assert'),
     util = require(path.resolve(__dirname, 'util'));
 
 describe('nemo-view @methods@', function () {
-  before(function (done) {
-    nemo = Nemo(done);
+  before(async function() {
+    nemo = await Nemo();
   });
-  after(function (done) {
-    nemo.driver.quit().then(done);
+
+  after(async function() {
+    await nemo.driver.quit();
   });
-  beforeEach(function (done) {
-    nemo.driver.get(nemo.data.baseUrl);
-    util.waitForJSReady(nemo).then(util.doneSuccess(done), util.doneError(done));
+
+  beforeEach(async function() {
+    await nemo.driver.get(nemo.data.baseUrl);
+    await util.waitForJSReady(nemo);
   });
-  it('should return a @locatorObject@', function (done) {
+  
+  it('should return a @locatorObject@', async function () {
     var locator = nemo.view.simple.outBoxBy();
-    if (locator.using && locator.value) {
-      done();
-    } else {
-      done(new Error('didnt get back a locator object'));
-    }
+    assert(locator.using,'didnt get back a locator object');
+    assert(locator.value,'didnt get back a locator object');
   });
-  it('should find an existing element using the @Wait@positive@ method', function (done) {
-    nemo.view.simple.bodyTag.wait(3000, 'didnt find body tag').getTagName().then(function (tn) {
-      if (tn.toLowerCase() !== 'body') {
-        done(new Error('something went wrong here'));
-      }
+  it('should find an existing element using the @Wait@positive@ method', async function () {
+    await nemo.view.simple.bodyTag.wait(3000, 'didnt find body tag').getTagName().then(function (tn) {
+      assert.equal(tn.toLowerCase(), 'body', 'something went wrong here');
     });
-    nemo.view.simple.bodyTagWait(3000, 'didnt find body tag').getTagName().then(function (tn) {
-      if (tn.toLowerCase() === 'body') {
-        done();
-      } else {
-        done(new Error('something went wrong here'));
-      }
-    }, util.doneError(done));
+    await nemo.view.simple.bodyTagWait(3000, 'didnt find body tag').getTagName().then(function (tn) {
+      assert.equal(tn.toLowerCase(), 'body', 'something went wrong here');
+    });
   });
-  it('should appropriately use a timeout argument to the @Wait@negative@CustomTimeout@ method in a failure scenario', function (done) {
+  it('should appropriately use a timeout argument to the @Wait@negative@CustomTimeout@ method in a failure scenario', async function () {
     var start = Date.now();
-    nemo.view.simple.notExistWait(13000, 'didnt find notExist').then(function (find) {
-      done(new Error('found notExist but should not have'));
-    }, function (err) {
-      var found = Date.now() - start;
-      console.log('timeout in ', found);
-      if (found > 13800 || found < 12500) {
-        done(new Error('error thrown but in the wrong period of time, '));
-      } else {
-        done();
+    let err;
+    await nemo.view.simple.notExistWait(13000, 'didnt find notExist')
+    .catch(error => {err = error})
+    .finally(() => {
+      if(err){
+        var found = Date.now() - start;
+        if (found > 13800 || found < 12500) {
+          assert.fail('error thrown but in the wrong period of time, ')
+        }
       }
-    });
+      else {
+        assert.fail('found notExist but should not have')
+      }
+    })
+
   });
-  it('should appropriately use a DIFFERENT timeout argument to the @Wait@negative@CustomTimeout@ method in a failure scenario', function (done) {
+  it('should appropriately use a DIFFERENT timeout argument to the @Wait@negative@CustomTimeout@ method in a failure scenario', async function () {
     var start = Date.now();
-    nemo.view.simple.notExistWait(3000, 'didnt find notExist').then(function (find) {
-      done(new Error('found notExist but should not have'));
-    }, function (err) {
-      var found = Date.now() - start;
-      console.log('timeout in ', found);
-      if (found > 3800 || found < 2500) {
-        done(new Error('error thrown but in the wrong period of time, '));
-      } else {
-        done();
+    let err;
+    await nemo.view.simple.notExistWait(3000, 'didnt find notExist')
+    .catch(error => {err = error})
+    .finally(() => {
+      if(err){
+        var found = Date.now() - start;
+        if (found > 3800 || found < 2500) {
+          assert.fail('error thrown but in the wrong period of time, ')
+        }
+      }
+      else {
+        assert.fail('found notExist but should not have')
       }
     });
+    
   });
-  it('should use @WaitVisible@positive@ method', function (done) {
-    nemo.driver.get(nemo.data.baseUrl + '/waits');
-    util.waitForJSReady(nemo);
-    nemo.view.simple.waitButton().click();
-    nemo.view.simple.outBox.waitVisible();
-    nemo.view.simple.outBoxWaitVisible(6000, 'didnt find outbox').getTagName().then(function (tn) {
+  it('should use @WaitVisible@positive@ method', async function () {
+    await nemo.driver.get(nemo.data.baseUrl + '/waits');
+    await util.waitForJSReady(nemo);
+    await nemo.view.simple.waitButton().click();
+    await nemo.view.simple.outBox.waitVisible();
+    await nemo.view.simple.outBoxWaitVisible(6000, 'didnt find outbox').getTagName().then(function (tn) {
       assert.equal(tn.toLowerCase(), 'div');
-      done();
-    }, util.doneError(done));
+    });
   });
-  it('should use @WaitVisible@negative@ method for element present but not visible', function (done) {
+  it('should use @WaitVisible@negative@ method for element present but not visible', async function () {
+    var start;
+    await nemo.driver.get(nemo.data.baseUrl + '/waits');
+    await util.waitForJSReady(nemo).then(function () {
+      start = Date.now();
+    });
+
+    await nemo.view.simple.outBoxWaitVisible(3000, 'didnt find outbox')
+    .catch(error => {err = error})
+    .finally(() => {
+      if(err){
+        var found = Date.now() - start;
+        if (found > 3800 || found < 2500) {
+          assert.fail('error thrown but in the wrong period of time, ')
+        }
+      }
+      else {
+        assert.fail('shouldn\'t have found the element to be visible')
+      }
+    })
+  });
+  it('should use @WaitVisible@negative@ method for element not present ', async function () {
     var start;
     nemo.driver.get(nemo.data.baseUrl + '/waits');
     util.waitForJSReady(nemo).then(function () {
@@ -96,25 +118,7 @@ describe('nemo-view @methods@', function () {
       }
     });
   });
-  it('should use @WaitVisible@negative@ method for element not present ', function (done) {
-    var start;
-    nemo.driver.get(nemo.data.baseUrl + '/waits');
-    util.waitForJSReady(nemo).then(function () {
-      start = Date.now();
-    });
-    nemo.view.simple.outBoxWaitVisible(3000, 'didnt find outbox').then(function (find) {
-      done(new Error('shouldn\'t have found the element to be visible'));
-    }, function (err) {
-      var found = Date.now() - start;
-      console.log('timeout in ', found);
-      if (found > 3800 || found < 2500) {
-        done(new Error('error thrown but in the wrong period of time, '));
-      } else {
-        done();
-      }
-    });
-  });
-  it('should return true/false using @Visible@Positive@ method when element present', function (done) {
+  it('should return true/false using @Visible@Positive@ method when element present', async function () {
     nemo.view.simple.outBoxVisible().then(function (visible) {
       assert.equal(visible, false);
     });
@@ -125,10 +129,10 @@ describe('nemo-view @methods@', function () {
       assert.equal(visible, true);
     }).then(util.doneSuccess(done), util.doneError(done));
   });
-  it('should throw error using @Visible@Negative@ method when element not present', function (done) {
+  it('should throw error using @Visible@Negative@ method when element not present', async function () {
     nemo.view.simple.notExistVisible().then(util.doneError(done), util.doneSuccess(done));
   });
-  it('should return true using @Present@Positive@ method', function (done) {
+  it('should return true using @Present@Positive@ method', async function () {
     nemo.view.simple.outBox.present().then(function (present) {
       assert.equal(present, true);
     });
@@ -137,13 +141,13 @@ describe('nemo-view @methods@', function () {
       done();
     }, util.doneError(done));
   });
-  it('should return false using @Present@negative@ method', function (done) {
+  it('should return false using @Present@negative@ method', async function () {
     nemo.view.simple.notExistPresent().then(function (present) {
       assert.equal(present, false);
       done();
     }, util.doneError(done));
   });
-  it('should return true using @TextEquals@Positive@ method', function (done) {
+  it('should return true using @TextEquals@Positive@ method', async function () {
     nemo.view.simple.pageHeader.textEquals('Sample form stuff').then(function (isEqual) {
       assert.equal(isEqual, true);
     });
@@ -152,14 +156,14 @@ describe('nemo-view @methods@', function () {
       done();
     }, util.doneError(done));
   });
-  it('should return false using @TextEquals@negative@ method', function (done) {
+  it('should return false using @TextEquals@negative@ method', async function () {
     nemo.view.simple.pageHeaderTextEquals('form stuff').then(function () {
       done(new Error('this promise should have been rejected'));
     }, function (err) {
       done();
     });
   });
-  it('should return true using @AttrEquals@Positive@ method', function (done) {
+  it('should return true using @AttrEquals@Positive@ method', async function () {
     nemo.view.simple.buttonLabel.attrEquals('value', 'Go foo').then(function (isEqual) {
       assert.equal(isEqual, true);
     });
@@ -168,7 +172,7 @@ describe('nemo-view @methods@', function () {
       done();
     }, util.doneError(done));
   });
-  it('should return false using @AttrEquals@negative@ method', function (done) {
+  it('should return false using @AttrEquals@negative@ method', async function () {
     nemo.view.simple.buttonLabelAttrEquals('value', 'foo').then(function () {
       done(new Error('this promise should have been rejected'));
     }, function (err) {
@@ -176,7 +180,7 @@ describe('nemo-view @methods@', function () {
     });
   });
   //GENERIC methods
-  it('should resolve true if element exists @_present@positive@ method', function (done) {
+  it('should resolve true if element exists @_present@positive@ method', async function () {
     nemo.view._present('body').then(function (found) {
       if (found) {
         done();
@@ -185,7 +189,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should resolve true if element exists @_present@withParent@positive@ method', function (done) {
+  it('should resolve true if element exists @_present@withParent@positive@ method', async function () {
     nemo.view._present('label[for="foo_text"]', nemo.view.simple.form()).then(function (found) {
       if (found) {
         done();
@@ -196,7 +200,7 @@ describe('nemo-view @methods@', function () {
   });
 
 
-  it('should resolve false if element doesn\'t exists @_present@negative@ method', function (done) {
+  it('should resolve false if element doesn\'t exists @_present@negative@ method', async function () {
     nemo.view._present('booody').then(function (found) {
       if (!found) {
         done();
@@ -205,7 +209,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should resolve false if element does not exists @_present@withParent@negative@ method', function (done) {
+  it('should resolve false if element does not exists @_present@withParent@negative@ method', async function () {
     nemo.view._present('boooddy', nemo.view.simple.form()).then(function (found) {
       if (found) {
         done(new Error('something went wrong here'));
@@ -214,7 +218,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should resolve true if element visible @_visible@positive@ method', function (done) {
+  it('should resolve true if element visible @_visible@positive@ method', async function () {
     nemo.view._visible('body').then(function (visible) {
       if (visible) {
         done();
@@ -223,7 +227,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should resolve true if element visible @_visible@withParent@positive@ method', function (done) {
+  it('should resolve true if element visible @_visible@withParent@positive@ method', async function () {
     nemo.view._visible('label[for="foo_text"]', nemo.view.simple.form()).then(function (visible) {
       if (visible) {
         done();
@@ -232,7 +236,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should resolve false if element not visible @_visible@negative@ method', function (done) {
+  it('should resolve false if element not visible @_visible@negative@ method', async function () {
     nemo.view._visible('id:outy').then(function (visible) {
       if (!visible) {
         done();
@@ -241,7 +245,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should resolve false if element not visible @_visible@withParent@negative@ method', function (done) {
+  it('should resolve false if element not visible @_visible@withParent@negative@ method', async function () {
     nemo.view._visible('id:outy', nemo.view.simple.form()).then(function (visible) {
       if (!visible) {
         done();
@@ -250,7 +254,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should find an existing element using the @_find@positive@ method', function (done) {
+  it('should find an existing element using the @_find@positive@ method', async function () {
     nemo.view._find('body').getTagName().then(function (tn) {
       if (tn.toLowerCase() === 'body') {
         done();
@@ -259,7 +263,7 @@ describe('nemo-view @methods@', function () {
       }
     }, util.doneError(done));
   });
-  it('should find an existing element using the @_find@withparent@positive@ method', function (done) {
+  it('should find an existing element using the @_find@withparent@positive@ method', async function () {
     nemo.view._find('label[for="foo_text"]', nemo.view.simple.form()).then(function (element) {
       element.getText().then(function(text){
         if(text === 'foo Text:') {
@@ -271,12 +275,12 @@ describe('nemo-view @methods@', function () {
     }, util.doneError(done));
   });
 
-  it('should throw error for non-present element with @_find@negative@ method', function (done) {
+  it('should throw error for non-present element with @_find@negative@ method', async function () {
     nemo.view._find('booody').then(function () {
       done(new Error('should not have found an element'));
     }, util.doneSuccess(done));
   });
-  it('should find an array of elements using the @_finds@positive@ method', function (done) {
+  it('should find an array of elements using the @_finds@positive@ method', async function () {
     nemo.view._finds('input[type=text]').then(function (inputs) {
       var promises = [];
       inputs.forEach(function (input, idx) {
@@ -298,23 +302,17 @@ describe('nemo-view @methods@', function () {
       done();
     }, util.doneError(done));
   });
-  it('should find elements using @_finds@withParent@ method', function (done) {
-    nemo.view._finds('div.fielder', nemo.view.simple.form()).then(function (divs) {
-      if (divs.length === 4) {
-        done();
-      }
-    }, util.doneError(done));
+  it('should find elements using @_finds@withParent@ method', async function () {
+    await nemo.view._finds('div.fielder', await nemo.view.simple.form()).then(function (divs) {
+      assert.equal(divs.length, 4)
+    });
   });
-  it('should find an existing element using the @_wait@positive@ method', function (done) {
-    nemo.view._wait('body', 3000).getTagName().then(function (tn) {
-      if (tn.toLowerCase() === 'body') {
-        done();
-      } else {
-        done(new Error('something went wrong here'));
-      }
-    }, util.doneError(done));
+  it('should find an existing element using the @_wait@positive@ method', async function () {
+    await nemo.view._wait('body', 3000).getTagName().then(function (tn) {
+      assert.equal(tn.toLowerCase(), 'body')
+    });
   });
-  it('should appropriately use a timeout argument to the @_wait@negative@CustomTimeout@ method in a failure scenario', function (done) {
+  it('should appropriately use a timeout argument to the @_wait@negative@CustomTimeout@ method in a failure scenario', async function () {
     var start = Date.now(), msg = 'Element did not load for specified timeout';
     nemo.view._wait('bordy.foo.blarg', 13000, msg).then(function (find) {
       done(new Error('found notExist but should not have'));
@@ -329,7 +327,7 @@ describe('nemo-view @methods@', function () {
       }
     });
   });
-  it('should appropriately use a DIFFERENT timeout argument to the @_wait@negative@CustomTimeout@ method in a failure scenario', function (done) {
+  it('should appropriately use a DIFFERENT timeout argument to the @_wait@negative@CustomTimeout@ method in a failure scenario', async function () {
     var start = Date.now(), msg = 'Element did not load for specified timeout';
     nemo.view._wait('bordy.foo.blarg', 3000, msg).then(function (find) {
       done(new Error('found notExist but should not have'));
@@ -344,7 +342,7 @@ describe('nemo-view @methods@', function () {
       }
     });
   });
-  it('should use @_waitVisible@positive@ method', function (done) {
+  it('should use @_waitVisible@positive@ method', async function () {
     nemo.driver.get(nemo.data.baseUrl + '/waits');
     util.waitForJSReady(nemo);
     nemo.view.simple.waitButton().click();
@@ -353,7 +351,7 @@ describe('nemo-view @methods@', function () {
       done();
     }, util.doneError(done));
   });
-  it('should use @_waitVisible@negative@ method for element present but not visible', function (done) {
+  it('should use @_waitVisible@negative@ method for element present but not visible', async function () {
     var start, msg = 'Element did not load for specified timeout';
     nemo.driver.get(nemo.data.baseUrl + '/waits');
     util.waitForJSReady(nemo).then(function () {
@@ -372,7 +370,7 @@ describe('nemo-view @methods@', function () {
       }
     });
   });
-  it('should use @_waitVisible@negative@ method for element not present ', function (done) {
+  it('should use @_waitVisible@negative@ method for element not present ', async function () {
     var start, msg = 'Element did not load for specified timeout';
     nemo.driver.get(nemo.data.baseUrl + '/waits');
     util.waitForJSReady(nemo).then(function () {
@@ -391,7 +389,7 @@ describe('nemo-view @methods@', function () {
       }
     });
   });
-  it('should use @_firstVisible@positive@ method to find an element which isnt initially visible', function (done) {
+  it('should use @_firstVisible@positive@ method to find an element which isnt initially visible', async function () {
     nemo.driver.get(nemo.data.baseUrl + '/waits');
     util.waitForJSReady(nemo);
     nemo.view.simple.waitButton().click();
@@ -405,7 +403,7 @@ describe('nemo-view @methods@', function () {
       done();
     }, util.doneError(done));
   });
-  it('should use @_firstVisible@negative@ method to throw error when no elements found', function (done) {
+  it('should use @_firstVisible@negative@ method to throw error when no elements found', async function () {
     var start;
     nemo.driver.get(nemo.data.baseUrl + '/waits');
     util.waitForJSReady(nemo).then(function () {
